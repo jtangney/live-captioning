0a204b44b2d1968ef6d2f5df1cf80644a24f5ebc
jtangney <jeremy.tangney@gmail.com>
2019-12-09 15:48:18 +0000

    Send the audio data to Redis from a background task

--- ingest/socketio_ingest.py
+++ ingest/socketio_ingest.py
@@ -1,9 +1,14 @@
 import argparse
 import base64
+import queue
 
-import redis
+import eventlet
+
+eventlet.monkey_patch()
 from flask import Flask
 from flask_socketio import SocketIO
+import redis
+
 
 parser = argparse.ArgumentParser()
 parser.add_argument("--host", default="localhost")
@@ -14,26 +19,48 @@ parser.add_argument("--id", default="Ingest")
 args = parser.parse_args()
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'secret!'
 socketio = SocketIO()
 rdb = redis.Redis(host=args.redisHost, port=6379, db=0,
                   health_check_interval=2, socket_timeout=4)
-buffer = []
-
-
-@app.route('/')
-def hello_world():
-  return 'hello from ' + args.id
+buff = queue.Queue()
+connected = False
 
 
 @socketio.on('connect')
 def connect():
   print('%s socket connected!' % args.id)
+  global connected
+  connected = True
 
 
 @socketio.on('disconnect')
-def connect():
+def disconnect():
   print('%s socket disconnected!' % args.id)
+
+
+@socketio.on('data')
+def handle_data(data):
+  """Stores the received audio data in a local buffer"""
+  encoded = base64.b64encode(data)
+  buff.put(encoded, block=False)
+
+
+def _enqueue_audio(redis_queue):
+  """Blocking-reads data from the buffer and adds to Redis queue"""
+  while True:
+    if not connected:
+      socketio.sleep(0.1)
+      continue
+
+    try:
+      chunk = buff.get(block=True)
+      val = rdb.lpush(redis_queue, chunk)
+      # debugging; under normal circumstances audio should not be accumulating
+      if val > 1:
+        print('Ingested audio queue length: %d' % val)
+    except redis.exceptions.RedisError as err:
+      print('Error pushing into Redis queue: %s' % err)
+      socketio.sleep(0.5)
 
 
 @socketio.on('message')
@@ -41,24 +68,13 @@ def handle_message(message):
   print('Ignoring received string message: ' + message)
 
 
-@socketio.on('data')
-def handle_data(data):
-  encoded = base64.b64encode(data)
-  try:
-    # send buffered audio first if it exists
-    if len(buffer) > 0:
-      for elem in buffer:
-        rdb.lpush(args.redisQueue, elem)
-      buffer.clear()
-      print('Sent buffered audio!')
-    val = rdb.lpush(args.redisQueue, encoded)
-    if val > 1:
-      print(val)
-  except redis.exceptions.RedisError as err:
-    print('Error pushing into Redis queue: %s' % err)
-    buffer.append(encoded)
+@app.route('/')
+def hello_world():
+  return 'hello from ' + args.id
 
 
 if __name__ == '__main__':
   socketio.init_app(app)
+  socketio.start_background_task(_enqueue_audio, args.redisQueue)
   socketio.run(app, host=args.host, port=args.port)
+
