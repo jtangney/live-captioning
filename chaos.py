05bd1dc9428aaf6696ee23a00b97cf4e7d1aeef2
jtangney <jeremy.tangney@gmail.com>
2020-01-20 10:50:29 +0000

    added flag for killing multiple pods per iteration

--- chaos.py
+++ chaos.py
@@ -7,6 +7,7 @@ import time
 parser = argparse.ArgumentParser()
 parser.add_argument('--applabel', default=None)
 parser.add_argument('--count', default=1, type=int)
+parser.add_argument('--iterations', default=1, type=int)
 parser.add_argument('--delay', default=30, type=int)
 args = parser.parse_args()
 
@@ -24,16 +25,17 @@ def kill_pod(app_label):
   print(all_pods)
 
   pods = all_pods.split(' ')
-  victim = random.choice(pods)
-  print('killing pod: %s' % victim)
+  victims = random.sample(pods, min(args.count, len(pods)))
+  print('killing %d pod(s): %s' % (args.count, victims))
 
-  kill_pod_command = 'kubectl delete pod %s --force --grace-period=0' % victim
-  elems = kill_pod_command.split(' ')
-  result = subprocess.check_output(elems)
-  print('%s at %s' % (result.decode('utf-8'), time.strftime("%H:%M:%S", time.gmtime())))
+  for victim in victims:
+    kill_pod_command = 'kubectl delete pod %s --force --grace-period=0' % victim
+    elems = kill_pod_command.split(' ')
+    result = subprocess.check_output(elems)
+    print('%s at %s' % (result.decode('utf-8'), time.strftime("%H:%M:%S", time.gmtime())))
 
 
 kill_pod(args.applabel)
-for i in range(args.count - 1):
+for i in range(args.iterations - 1):
   time.sleep(args.delay)
   kill_pod(args.applabel)
